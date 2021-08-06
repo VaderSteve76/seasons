6210d200ec73c93047c6a16ee4c898bd9c7668f3
VaderSteve76 <alverson.s@yahoo.com>
2021-08-06 13:28:06 -0400

    Added constructor, super and setting state

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,6 +1,14 @@
 import { React, Component } from 'react';
 
 class SeasonDisplay extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null
+    };
+  }
+
   render() {
 
     window.navigator.geolocation.getCurrentPosition(
