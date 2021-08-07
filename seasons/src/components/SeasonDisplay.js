2dff129855f662d067763768bf423efd0aa65d74
VaderSteve76 <alverson.s@yahoo.com>
2021-08-07 13:13:09 -0400

    Added error message

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -5,20 +5,30 @@ class SeasonDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: null
+      lat: null,
+      errMsg: ''
     };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
+        console.log(position)
         this.setState({ lat: position.coords.latitude })
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errMsg: err.message });
+      }
     );
 
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br/>
+        {this.state.errMsg}
+      </div>
+    );
   }
 
 }
