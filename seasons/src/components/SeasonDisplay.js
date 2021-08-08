3a83f5c4d1936bd8e4f8871eeee3f076ffbb2969
VaderSteve76 <alverson.s@yahoo.com>
2021-08-08 14:41:18 -0400

    Refactored all functions inside class component

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,35 +1,41 @@
-import { React, Component } from 'react';
+import { React, Component } from "react";
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
+};
 
 class SeasonDisplay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errMsg: ''
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     lat: null,
+  //     errMsg: ''
+  //   };
+  state = { lat: null, errMsg: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errMsg: err.message })
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMsg: err.message })
     );
   }
 
   componentDidUpdate() {
-    console.log("Componenet updated")
+    console.log("Component updated");
   }
 
   render() {
-    if(this.state.errMsg && !this.state.lat) {
-      return <div>Error: {this.state.errMsg}</div>
-    } 
-    if(!this.state.errMsg && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
-    }
-    if(!this.state.errMsg && !this.state.lat) {
-      return <div>Loading!</div>
-    }
+    return (
+      <div>
+        {getSeason === "winter"
+          ? "It's cold outside, wear a jacket"
+          : "Let's hit the beach"}
+      </div>
+    );
   }
 }
 
