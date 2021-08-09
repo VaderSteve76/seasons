912d5e9a94b2a8d6426c05d1919bb0a288fecd85
VaderSteve76 <alverson.s@yahoo.com>
2021-08-09 13:59:04 -0400

    Refactored logic to make better sense

--- seasons/src/components/SeasonDisplay.js
+++ seasons/src/components/SeasonDisplay.js
@@ -1,36 +1,41 @@
 import { React, Component } from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "It's cold, wear a jacket",
+    iconName: "snowflake"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "Winter" : "Summer";
+    return lat > 0 ? "winter" : "summer";
   }
 };
 class SeasonDisplay extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     lat: null,
-  //     errMsg: ''
-  //   };
   state = { lat: null, errMsg: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errMsg: err.message })
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMsg: err.message })
     );
   }
 
   render() {
     const season = getSeason(this.lat, new Date().getMonth());
-    const text = season === "winter" ? "It's cold outside, wear a jacket" : "Let's hit the beach";
-    const icon = season === "winter" ? "snowflake" : "sun";
+    const {text, iconName } = seasonConfig[season];
     return (
       <div id="season">
-        <i className={`${icon} icon extra large`} />
+        <i className={`${iconName} icon extra large`} />
         <h1>{text}</h1>
+        <i className={`${iconName} icon extra large`} />
       </div>
     );
   }
